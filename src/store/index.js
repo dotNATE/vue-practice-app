a0514d01e3745c3eb848bdc159966114ff028c24
dotNate <somersetnathaniel@gmail.com>
2021-11-03 21:53:46 +0000

    added validation to SimpleForm.vue and moved data to GStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,19 @@
 import { reactive } from "vue";
 
-export default reactive({ flashMessage: "", event: null });
+export default reactive({
+  flashMessage: "",
+  event: null,
+  categories: [
+    "sustainability",
+    "nature",
+    "animal welfare",
+    "housing",
+    "education",
+    "food",
+    "community",
+  ],
+  petOptions: [
+    { label: "Yes", value: 1 },
+    { label: "No", value: 0 },
+  ],
+});
--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <h1>Create an event</h1>
-    <form @submit.prevent="sendForm">
+    <form @submit="sendForm">
       <BaseSelect
         :options="categories"
-        v-model="event.category"
+        v-model="values.category"
         label="Select a category"
       />
 
       <fieldset>
         <legend>Name & describe your event</legend>
 
-        <BaseInput v-model="event.title" label="Title" type="text" />
+        <BaseInput v-model="values.title" label="Title" type="text" />
 
         <BaseInput
-          v-model="event.description"
+          v-model="values.description"
           label="Description"
           type="text"
         />
@@ -22,14 +22,14 @@
       <fieldset>
         <legend>Where is your event?</legend>
 
-        <BaseInput v-model="event.location" label="Location" type="text" />
+        <BaseInput v-model="values.location" label="Location" type="text" />
       </fieldset>
       <fieldset>
         <legend>Pets</legend>
         <p>Are pets allowed?</p>
         <div>
           <BaseRadioGroup
-            v-model="event.pets"
+            v-model="values.pets"
             name="pets"
             :options="petOptions"
             vertical
@@ -40,11 +40,11 @@
       <fieldset>
         <legend>Extras</legend>
         <div>
-          <BaseCheckbox v-model="event.extras.catering" label="Catering" />
+          <BaseCheckbox v-model="values.catering" label="Catering" />
         </div>
 
         <div>
-          <BaseCheckbox v-model="event.extras.music" label="Live music" />
+          <BaseCheckbox v-model="values.music" label="Live music" />
         </div>
       </fieldset>
 
@@ -54,7 +54,11 @@
 </template>
 
 <script>
+import { inject } from "vue";
 import axios from "axios";
+
+import { useForm } from "vee-validate";
+import { object, string, number, boolean } from "yup";
 
 import BaseInput from "@/components/inputs/BaseInput.vue";
 import BaseSelect from "@/components/inputs/BaseSelect.vue";
@@ -62,44 +66,47 @@ import BaseCheckbox from "@/components/inputs/BaseCheckbox.vue";
 import BaseRadioGroup from "@/components/inputs/BaseRadioGroup.vue";
 
 export default {
-  data() {
-    return {
-      categories: [
-        "sustainability",
-        "nature",
-        "animal welfare",
-        "housing",
-        "education",
-        "food",
-        "community",
-      ],
-      event: {
-        category: "",
-        title: "",
-        description: "",
-        location: "",
-        pets: 1,
-        extras: {
-          catering: false,
-          music: false,
-        },
-      },
-      petOptions: [
-        { label: "Yes", value: 1 },
-        { label: "No", value: 0 },
-      ],
+  setup() {
+    const { categories, petOptions } = inject("GStore");
+
+    const validationSchema = object({
+      title: string().required(),
+      description: string().required(),
+      location: string().required(),
+      category: string().required(),
+      pets: number(),
+      catering: boolean(),
+      music: boolean(),
+    });
+
+    const initialValues = {
+      pets: 0,
+      catering: false,
+      music: false,
     };
-  },
-  methods: {
-    sendForm() {
+
+    const { values, errors, handleSubmit } = useForm({
+      validationSchema,
+      initialValues,
+    });
+
+    const sendForm = handleSubmit(values => {
       axios
         .post(
           "https://my-json-server.typicode.com/Code-Pop/Vue-3-Forms/events",
-          this.event
+          values
         )
         .then(response => console.log("Response", response))
         .catch(error => console.log("Error", error));
-    },
+    });
+
+    return {
+      categories,
+      petOptions,
+      sendForm,
+      values,
+      errors,
+    };
   },
   components: {
     BaseInput,
